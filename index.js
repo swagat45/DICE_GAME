9e211020641cd02408ad22313e2c84975b9e875f
swagat45 <124488676+swagat45@users.noreply.github.com>
2024-04-08 20:07:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var randomnumber1=Math.floor(Math.random() * 6) + 1;
+var randomdiceimage="dice" + randomnumber1 + ".png";
+var randomimagesource="images/" + randomdiceimage;
+
+var image1= document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomimagesource);
+
+var randomnumber2=Math.floor(Math.random() * 6) + 1;
+var randomimagesource2="images/dice" + randomnumber2 +".png";
+
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomimagesource2);
+
+if(randomnumber1 > randomnumber2){
+    document.querySelector("h1").innerHTML="🚩Player1 Wins!!";
+}
+else if(randomnumber1 < randomnumber2){
+    document.querySelector("h1").innerHTML="Player2 Wins!!🚩";
+}
+else{
+    document.querySelector("h1").innerHTML="DRAW!!";
+}
